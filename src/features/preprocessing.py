e008432852363c7022ca04a20cc97b67e9bf90a2
guilhermedom <guilhermedfs@gmail.com>
2022-06-30 23:54:30 -0300

    Refactor for better readability

--- src/features/preprocessing.py
+++ src/features/preprocessing.py
@@ -5,9 +5,9 @@ import shutil
 from os.path import join
 from os import listdir, path
 import os
-import numpy as np
 import random
 import math
+import numpy as np
 
 from matplotlib import pyplot as plt
 
@@ -22,13 +22,13 @@ from scipy import ndimage
 # path variables
 orlfacesdir = '../../data/raw/OrlFaces20'
 pessoalicmcdir = '../../data/raw/PessoasICMC'
-preprocessed = 'data/processed/'
-pre_orl = preprocessed + 'orl'
-pre_icmc = preprocessed + 'icmc'
-pre_icmc_images = pre_icmc + '/images'
-pre_orl_images = pre_orl + '/images'
-pre_icmc_hogs = pre_icmc + '/hog'
-pre_orl_hogs = pre_orl + '/hog'
+preprocessed = '../../data/processed/'
+pre_orl = preprocessed + 'orl/'
+pre_icmc = preprocessed + 'icmc/'
+pre_icmc_images = pre_icmc + 'images'
+pre_orl_images = pre_orl + 'images'
+pre_icmc_hogs = pre_icmc + 'hog'
+pre_orl_hogs = pre_orl + 'hog'
 
 # plot or not the generated images
 PLOT = False
@@ -38,18 +38,18 @@ def gen_data(base_image, quantity, label):
     data = []
     labels = []
 	# for all input images
-    for i in range(0, quantity):
-        a = random.uniform(-10, 10)
-        b = random.randint(10, 20)
+    for _ in range(0, quantity):
+        pepper_seed = random.randint(10, 20)
+        rotate_degrees = random.uniform(-10, 10)
 		
 		# resize image
         img = transform.resize(base_image, (112, 92), anti_aliasing=True)
 		
 		# add random pepper effect as noise to the image
-        img = random_noise(img, mode='pepper', seed=b, clip=True)
+        img = random_noise(img, mode='pepper', seed=pepper_seed, clip=True)
 		
 		# randomly rotate image by a maximum of 10 degrees
-        img = transform.rotate(img, a, resize=False, center=None, order=1, mode='constant', cval=1,
+        img = transform.rotate(img, rotate_degrees, resize=False, center=None, order=1, mode='constant', cval=1,
                                clip=True, preserve_range=True)
         # add blur effect to the image
         img = ndimage.gaussian_filter(img, sigma=2)
@@ -59,14 +59,14 @@ def gen_data(base_image, quantity, label):
 		
     return data, labels
 
-# carrega as imagens presentes em determinado path
+# load all images in input path
 def load_images(path):
     images = []
     labels = []
     dictionary = {}
 	
-	# percorre os subdiretórios e carrega as imagens presentes neles
-    for k, d in enumerate(listdir(path)):
+	# run through all subdirectories and collect their images
+    for _, d in enumerate(listdir(path)):
         if d in dictionary.values():
             l = dictionary[d]
         else:
@@ -79,7 +79,7 @@ def load_images(path):
             labels.append(l)
     return images, labels, dictionary
 
-# exibe as imagens presentes no array de entrada, em uma matriz com linhas = rows e colunas = columns
+# plot images in input array. Images are put together in a matrix
 def plot_images(array, rows, columns):
     fig = plt.figure(figsize=(columns * 2, rows * 2))
     for i, j in enumerate(array):
@@ -91,97 +91,101 @@ def plot_images(array, rows, columns):
 
     plt.show()
 
-# gera os feature descriptors e visualização dos HOGS
+# generate HOG feature descriptors and their visualization
 def hog_images(imgs):
-	# lista de descriptors e visualizações HOG
-    fds, hog_imgs = [], []
+    feature_descriptors_list, hog_imgs = [], []
     for i, image in enumerate(imgs):
-		# gera os HOGs com a seguinte configuração, que gera as imagens rapidamente sem interferir na sua qualidade e por consequência, nos classificadores
-        fd, hog_image = hog(image, orientations=9, pixels_per_cell=(4, 4),
+		# generate HOG features using a specific configuration that creates them fast 
+        # without causing performance loss in classifiers
+        feature_descriptors, hog_image = hog(image, orientations=9, pixels_per_cell=(4, 4),
                             cells_per_block=(1, 1), visualize=True, multichannel=False,
                             transform_sqrt=True, block_norm="L1")
         hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
-        fds.append(fd)
+        feature_descriptors_list.append(feature_descriptors)
         hog_imgs.append(hog_image_rescaled)
-        print('\r\tHOG %d/%d generated' % (i+1, len(imgs)), end='')
+        print('\r\tHOG %d/%d generated' % (i + 1, len(imgs)), end='')
     print()
-    return fds, hog_imgs
+    return feature_descriptors_list, hog_imgs
+
+def main():
+    print('Loading base dataset icmc')
+    images, labels_a, dictionary_icmc = load_images(pessoalicmcdir)
+    # plot images if PLOT constant is set to true
+    if PLOT:
+        plot_images(images, len(images) / 4, 4)
+        print(labels_a)
+    print('Dataset loaded')
+
+    print('Generating dataset icmc')
+    dataset_icmc, labels_icmc = [], []
+    for im, l in zip(images, labels_a):
+        a, b = gen_data(im, 10, l)
+        dataset_icmc += a
+        labels_icmc += b
+    if PLOT:
+        plot_images(dataset_icmc, math.ceil(len(dataset_icmc) / 4), 4)
+        print(labels_icmc)
+    print('Dataset generated')
+
+    print('Loading dataset orl')
+    dataset_orl, labels_orl, dictionary_orl = load_images(orlfacesdir)
+    if PLOT:
+        plot_images(dataset_orl, math.ceil(len(dataset_orl) / 4), 4)
+        print(labels_orl)
+    print('Dataset loaded')
 
 
-print('Loading base dataset icmc')
-images, labels_a, dictionary_icmc = load_images(pessoalicmcdir)
-# se a constante PLOT estiver com valor True, exibe as imagens
-if PLOT:
-    plot_images(images, len(images) / 4, 4)
-    print(labels_a)
-print('Dataset loaded')
-
-print('Generating dataset icmc')
-dataset_icmc, labels_icmc = [], []
-for im, l in zip(images, labels_a):
-    a, b = gen_data(im, 10, l)
-    dataset_icmc += a
-    labels_icmc += b
-if PLOT:
-    plot_images(dataset_icmc, math.ceil(len(dataset_icmc) / 4), 4)
-    print(labels_icmc)
-print('Dataset generated')
-
-print('Loading dataset orl')
-dataset_orl, labels_orl, dictionary_orl = load_images(orlfacesdir)
-if PLOT:
-    plot_images(dataset_orl, math.ceil(len(dataset_orl) / 4), 4)
-    print(labels_orl)
-print('Dataset loaded')
+    print('Generating HOG icmc')
+    feature_descriptors_icmc, hog_icmc = hog_images(dataset_icmc)
+    if PLOT:
+        plot_images(hog_icmc, math.ceil(len(hog_icmc) / 4), 4)
+    print('HOG generated')
 
 
-print('Generating HOG icmc')
-fd_icmc, hog_icmc = hog_images(dataset_icmc)
-if PLOT:
-    plot_images(hog_icmc, math.ceil(len(hog_icmc) / 4), 4)
-print('HOG generated')
+    print('Generating HOG orl')
+    feature_descriptors_orl, hog_orl = hog_images(dataset_orl)
+    if PLOT:
+        plot_images(hog_orl, math.ceil(len(hog_orl) / 4), 4)
+    print('HOG generated')
+
+    # create directories, in case they do not exist, to save images
+    print('Creating output directories')
+    if path.exists(preprocessed):
+        shutil.rmtree(preprocessed)
+    os.mkdir(preprocessed)
+    os.mkdir(pre_icmc)
+    os.mkdir(pre_orl)
+    os.mkdir(pre_icmc_images)
+    os.mkdir(pre_orl_images)
+    os.mkdir(pre_icmc_hogs)
+    os.mkdir(pre_orl_hogs)
+    print('Directories created')
+
+    print('Saving icmc images')
+    for i, im, lb in zip(range(len(dataset_icmc)), dataset_icmc, labels_icmc):
+        if not path.exists(pre_icmc_images + '/p' + str(lb)):
+            os.mkdir(pre_icmc_images + '/p' + str(lb))
+        plt.imsave(pre_icmc_images + '/p' + str(lb) + '/' + str(i) + '.png', im, cmap=plt.cm.gray)
+    print('Images saved')
+
+    print('Saving orl images')
+    for i, im, lb in zip(range(len(dataset_orl)), dataset_orl, labels_orl):
+        if not path.exists(pre_orl_images + '/p' + str(lb)):
+            os.mkdir(pre_orl_images + '/p' + str(lb))
+        plt.imsave(pre_orl_images + '/p' + str(lb) + '/' + str(i) + '.png', im, cmap=plt.cm.gray)
+    print('Images saved')
 
 
-print('Generating HOG orl')
-fd_orl, hog_orl = hog_images(dataset_orl)
-if PLOT:
-    plot_images(hog_orl, math.ceil(len(hog_orl) / 4), 4)
-print('HOG generated')
+    print('Saving icmc HOG')
+    np.save(pre_icmc_hogs + '/fd', np.array(feature_descriptors_icmc))
+    np.save(pre_icmc_hogs + '/labels', np.array(labels_icmc))
+    print('HOG features and images saved')
 
-# cria os diretórios, caso eles não existam para salvar as imagens
-print('Creating output directories')
-if path.exists(preprocessed):
-    shutil.rmtree(preprocessed)
-os.mkdir(preprocessed)
-os.mkdir(pre_icmc)
-os.mkdir(pre_orl)
-os.mkdir(pre_icmc_images)
-os.mkdir(pre_orl_images)
-os.mkdir(pre_icmc_hogs)
-os.mkdir(pre_orl_hogs)
-print('Directories created')
-
-print('Saving icmc images')
-for i, im, lb in zip(range(len(dataset_icmc)), dataset_icmc, labels_icmc):
-    if not path.exists(pre_icmc_images + '/p' + str(lb)):
-        os.mkdir(pre_icmc_images + '/p' + str(lb))
-    plt.imsave(pre_icmc_images + '/p' + str(lb) + '/' + str(i) + '.png', im, cmap=plt.cm.gray)
-print('Images saved')
-
-print('Saving orl images')
-for i, im, lb in zip(range(len(dataset_orl)), dataset_orl, labels_orl):
-    if not path.exists(pre_orl_images + '/p' + str(lb)):
-        os.mkdir(pre_orl_images + '/p' + str(lb))
-    plt.imsave(pre_orl_images + '/p' + str(lb) + '/' + str(i) + '.png', im, cmap=plt.cm.gray)
-print('Images saved')
+    print('Saving orl HOG')
+    np.save(pre_orl_hogs + '/fd', np.array(feature_descriptors_orl))
+    np.save(pre_orl_hogs + '/labels', np.array(labels_orl))
+    print('HOG features and images saved')
 
 
-print('Saving icmc HOG')
-np.save(pre_icmc_hogs + '/fd', np.array(fd_icmc))
-np.save(pre_icmc_hogs + '/labels', np.array(labels_icmc))
-print('HOG saved')
-
-print('Saving orl HOG')
-np.save(pre_orl_hogs + '/fd', np.array(fd_orl))
-np.save(pre_orl_hogs + '/labels', np.array(labels_orl))
-print('HOG saved')
+if __name__ == "__main__":
+    main()
--- src/models/training.py
+++ src/models/training.py
@@ -1,173 +1,175 @@
+# Guilherme Domingos Faria Silva
+# Wallace Alves Esteves Manzano
+
+import warnings
+
+from statistics import mean
+
 from sklearn.model_selection import StratifiedKFold
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.neural_network import MLPClassifier
-# Guilherme Domingos Faria Silva NUSP: 9361094
-# Wallace Alves Esteves Manzano NUSP: 9790840
-
-import numpy as np
-from statistics import mean
 from sklearn.decomposition import PCA
 
-import warnings
+import numpy as np
+
 warnings.filterwarnings("ignore")
 
-# diretorios usados no código
-preprocessed = 'preprocessed_data/'
-pre_orl = preprocessed + 'orl'
-pre_icmc = preprocessed + 'icmc'
-pre_icmc_hogs = pre_icmc + '/hog'
-pre_orl_hogs = pre_orl + '/hog'
+# path variables
+preprocessed = '../../data/processed/'
+pre_orl = preprocessed + 'orl/'
+pre_icmc = preprocessed + 'icmc/'
+pre_icmc_hogs = pre_icmc + 'hog/'
+pre_orl_hogs = pre_orl + 'hog/'
 
-# numpy arrays usados no código
-fd_icmc = np.load(pre_icmc_hogs + '/fd.npy')
-fd_orl = np.load(pre_orl_hogs + '/fd.npy')
-labels_icmc = np.load(pre_icmc_hogs + '/labels.npy')
-labels_orl = np.load(pre_orl_hogs + '/labels.npy')
+# read numpy arrays from preprocessed images
+fd_icmc = np.load(pre_icmc_hogs + 'fd.npy')
+fd_orl = np.load(pre_orl_hogs + 'fd.npy')
+labels_icmc = np.load(pre_icmc_hogs + 'labels.npy')
+labels_orl = np.load(pre_orl_hogs + 'labels.npy')
 
-# configurações do PCA que mantém a variância por volta de 50%
+# PCA configuration that keep explained variance at around 50%, a percentage that does not disturb classifiers
 g_icmc = PCA(n_components=25)
 g_orl = PCA(n_components=50)
 pca_icmc = g_icmc.fit_transform(fd_icmc)
 pca_orl = g_orl.fit_transform(fd_orl)
 
-# função usada para testar os modelos recebidos como entrada no cross-validation, mais detalhes no relatório
+# test input models in a cross-validation manner
 def test(model, data, target):
-    # lista de acuracias para cada fold
-    acuracias = []
+    # accuracies list for each fold
+    accuracies = []
 
-    # instanciação do método com 10-fold
+    # instantiate 10-fold method
     skf_10 = StratifiedKFold(n_splits=10)
     i = 1
-    # percurso pelas 10 execuções
-    cm_t = np.zeros(shape=(20, 20))
+    # 20x20 matrix to save one confusion matrix for each one of the 20 classes from input
+    confusion_matrix_list = np.zeros(shape=(20, 20))
+
+    # goes through each fold
     for train_ind, test_ind in skf_10.split(data, target):
         print('\r\tFold %d' % i, end='')
         i += 1
 		
-		# divisão do dataset de treino e de teste e dos rótulos
         x_train, x_test = data[train_ind], data[test_ind]
         y_train, y_test = target[train_ind], target[test_ind]
 
-        # treina o modelo na repartição de treino do dataset
         model.fit(x_train, y_train)
 
-		# realiza a predição usando o modelo treinado acima
         y_pred = model.predict(x_test)
 
-		# calcula a acurácia e a matriz de confusão
         acc_score = accuracy_score(y_test, y_pred)
-        cm_t += confusion_matrix(y_test, y_pred)
-        acuracias.append(acc_score)
+        confusion_matrix_list += confusion_matrix(y_test, y_pred)
+        accuracies.append(acc_score)
     print('\r', end='')
 	
-	# retorna a lista de acurácias e uma matriz contendo todas as matrizes de confusão obtidas
-    return {'acc': acuracias, 'cm': cm_t}
+    return {'acc': accuracies, 'cm': confusion_matrix_list}
 
-# função para instanciação dos modelos com base nos melhores parâmetros obtidos nos testes
-# name = nome do dataset, data = dados de entrada, data_pcd = dados de dimensão reduzida, target = rótulos, pca = configuração do PCA
-def test_dataset(name, data, data_pcd, target, pca):
-    print('*************** TEST NO DATASET %s ***************' % name)
+# search for best models using a small grid search
+def test_dataset(dataset_name, data, data_reduced_dim, target, pca):
+    print('*************** TESTING IN DATASET %s ***************' % dataset_name)
     knn_results = {}
+
     # *********************** KNN ***********************
-	# inicializa o número de K ótimo, de maior acurácia
+	# instantiate optimal k, having the best accuracy
     kmax = None
-	# percorre os 3 valores para K recomendados
     for i, k in enumerate([3, 5, 7]):
         print('%d-NN (%d/3)' % (k, i + 1))
-		# chama a função test para executar o cross-validation no classificador e dados passados como argumento
+
+		# call cross-validation method
         knn_results[k] = test(KNeighborsClassifier(n_neighbors=k), data, target)
-		# calcula média das acurácias retornadas pela função test
+
+		# get the average accuracy for all folds
         acc = mean(knn_results[k]['acc'])
-        print('\tAcurácia média: %f' % acc)
-		# compara a acurácia dos classificadores para decidir o melhor K
+        print('\tAverage accuracy: %f' % acc)
+
         if kmax is None or mean(knn_results[kmax]['acc']) < acc:
             kmax = k
         print()
 
-    print('KNN com mair desempenho é com k = %d' % kmax)
-    print('Acurácia média: %f' % mean(knn_results[kmax]['acc']))
+    print('Best KNN has k = %d' % kmax)
+    print('Average accuracy: %f' % mean(knn_results[kmax]['acc']))
     cm = knn_results[kmax]['cm']
 	
-	# percorre todas as classes exibindo a precisão para cada uma
+	# show precision for all classes
     for i in range(20):
-		# a precisão é = TP / TP + FP
-        print('Precisão classe %d: %f' % (i+1, cm[i, i] / sum(cm[:, i])))
-    print('Matriz de confusão')
+		# precision = True Positives / True Positives + False Positives
+        print('Precision for class %d: %f' % (i+1, cm[i, i] / sum(cm[:, i])))
+    print('Confusion matrix')
     print(cm)
 
     # PCA
-	# chama a função test para fazer o cross-validation em determinado classificador, porém com o PCA aplicado no dataset
-    d = test(KNeighborsClassifier(n_neighbors=kmax), data_pcd, target)
+	# run cross-validation using KNN in the dataset with reduced dimensionality
+    d = test(KNeighborsClassifier(n_neighbors=kmax), data_reduced_dim, target)
 
-    print('KNN com PCA k = %d | # componentes = %d | variância coberta = %f' % (kmax, pca.n_components_,
+    print('KNN with PCA and k = %d | # components = %d | explained variance = %f' % (kmax, pca.n_components_,
                                                                                sum(pca.explained_variance_ratio_)))
-    print('Acurácia média: %f' % mean(d['acc']))
+    print('Average accuracy: %f' % mean(d['acc']))
     cm = d['cm']
-	# percorre todas as classes exibindo a precisão para cada uma (modo PCA)
+	# show precision for all classes (with PCA dimensionality reduction)
     for i in range(20):
-		# a precisão é = TP / TP + FP
-        print('Precisão classe %d: %f' % (i+1, cm[i, i] / sum(cm[:, i])))
-    print('Matriz de confusão')
+		# precision = True Positives / True Positives + False Positives
+        print('Precision for class %d: %f' % (i + 1, cm[i, i] / sum(cm[:, i])))
+    print('Confusion matrix')
     print(cm)
 
     # *********************** MLP ***********************
-	# inicializa o número de camadas ótimo, de maior acurácia
-    cmax = None
+	# instantiate best number of neurons per hidden layer, having the best accuracy
+    hl_max = None
     mlp_results = {}
     it = 0
-	# executa o MLP para as 4 configurações de camada abaixo, consideradas as melhores segundo os testes
-    for c in [(10,), (20,), (10, 5), (15, 10)]:
-		# executa o MLP para as 3 taxas de aprendizado consideradas as melhores segundo os testes
-        for n in [0.1, 0.5, 10]:
-			# executa o MLP para os 3 melhores momentums obtidos nos testes
-            for a in [0.1, 0.5, 0.9]:
-                ind = '%s | n = %f | a = %f' % (str(c), n, a)
+	# start grid search changing number of hidden layers, number of neurons, momentum and learning rate
+    for neurons_per_layer in [(10,), (20,), (10, 5), (15, 10)]:
+        for learning_rate in [0.1, 0.5, 10]:
+            for momentum in [0.1, 0.5, 0.9]:
+                ind = '%s | learning_rate = %f | momentum = %f' % (str(neurons_per_layer), learning_rate, momentum)
                 it += 1
-                print('MLP camada escondida %s  (%d/36)' % (ind, it))
-				# executa o cross-validation do MLP através da função test
-                mlp_results[ind] = test(MLPClassifier(solver='sgd', hidden_layer_sizes=c, learning_rate_init=n,
-                                                      momentum=a, random_state=1, max_iter=100, tol=1e-3), data, target)
-                mlp_results[ind]['c'] = c
-                mlp_results[ind]['n'] = n
-                mlp_results[ind]['a'] = a
+                print('MLP hidden layers %s  (%d/36)' % (ind, it))
+
+				# call cross-validation method
+                mlp_results[ind] = test(MLPClassifier(solver='sgd', hidden_layer_sizes=neurons_per_layer, learning_rate_init=learning_rate,
+                                                      momentum=momentum, random_state=1, max_iter=100, tol=1e-3), data, target)
+                mlp_results[ind]['neurons_per_layer'] = neurons_per_layer
+                mlp_results[ind]['learning_rate'] = learning_rate
+                mlp_results[ind]['momentum'] = momentum
+
+                # get the average accuracy for all folds
                 acc = mean(mlp_results[ind]['acc'])
-                print('\tAcurácia média: %f' % acc)
-				# determina a melhor configuração do MLP de acordo com a acuracia
-                if cmax is None or mean(mlp_results[cmax]['acc']) < acc:
-                    cmax = ind
+                print('Average accuracy: %f' % acc)
+
+                if hl_max is None or mean(mlp_results[hl_max]['acc']) < acc:
+                    hl_max = ind
                 print()
 
-    print('MLP com mair desempenho é com as camadas = %s' % str(cmax))
-    print('Acurácia média: %f' % mean(mlp_results[cmax]['acc']))
-    cm = mlp_results[cmax]['cm']
+    print('Best MLP has hidden layers = %s' % str(hl_max))
+    print('Average accuracy: %f' % mean(mlp_results[hl_max]['acc']))
+    cm = mlp_results[hl_max]['cm']
 	
-	# obtém a precisão para cada uma das 20 classes
+	# show precision for all classes
     for i in range(20):
-		# a precisão é = TP / TP + FP
-        print('Precisão classe %d: %f' % (i+1, cm[i, i] / sum(cm[:, i])))
-    print('Matriz de confusão')
+		# precision = True Positives / True Positives + False Positives
+        print('Precision for class %d: %f' % (i + 1, cm[i, i] / sum(cm[:, i])))
+    print('Confusion matrix')
     print(cm)
 
 
     # PCA
-	# executa o cross-validation do MLP através da função test, em modo PCA
-    d = test(MLPClassifier(solver='sgd', hidden_layer_sizes=mlp_results[cmax]['c'],
-                                          learning_rate_init=mlp_results[cmax]['n'], momentum=mlp_results[cmax]['a'],
-                                          random_state=1, max_iter=100, tol=1e-3), data_pcd, target)
+	# run cross-validation using KNN in the dataset with reduced dimensionality
+    d = test(MLPClassifier(solver='sgd', hidden_layer_sizes=mlp_results[hl_max]['neurons_per_layer'],
+                                          learning_rate_init=mlp_results[hl_max]['learning_rate'], momentum=mlp_results[hl_max]['momentum'],
+                                          random_state=1, max_iter=100, tol=1e-3), data_reduced_dim, target)
 
-    print('MLP com PCA camadas = %s | # componentes = %d | variância coberta = %f' %
-          (cmax, pca.n_components_, sum(pca.explained_variance_ratio_)))
-    print('Acurácia média: %f' % mean(d['acc']))
+    print('MLP with PCA and hidden layers = %s | # components = %d | explained variance = %f' %
+          (hl_max, pca.n_components_, sum(pca.explained_variance_ratio_)))
+    print('Average accuracy: %f' % mean(d['acc']))
     cm = d['cm']
-	# obtém a precisão para cada uma das 20 classes (modo PCA)
+	# show precision for all classes (with PCA dimensionality reduction)
     for i in range(20):
-		# a precisão é = TP / TP + FP
-        print('Precisão classe %d: %f' % (i+1, cm[i, i] / sum(cm[:, i])))
-    print('Matriz de confusão')
+		# precision = True Positives / True Positives + False Positives
+        print('Precision for class %d: %f' % (i+1, cm[i, i] / sum(cm[:, i])))
+    print('Confusion matrix')
     print(cm)
 
 
-# chama as funções iniciais para os dois datasets de imagens
+# run starting functions for both datasets
 test_dataset('ICMC', fd_icmc, pca_icmc, labels_icmc, g_icmc)
 test_dataset('ORL', fd_orl, pca_orl, labels_orl, g_orl)
